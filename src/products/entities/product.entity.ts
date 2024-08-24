4f778aca09b723b85af21967912d1916cf0b944f
thusspokedata <thusspokedata@protonmail.com>
2024-08-24 08:46:53 +0200

    feat: TypeORM - Entity - Product

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,12 @@
+// la entidad es una clase que representa una tabla (Product)en la base de datos
+
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', { unique: true })
+  title: string;
+}
